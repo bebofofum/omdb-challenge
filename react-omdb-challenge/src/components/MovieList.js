8733aa6447d177e177da681e61dce2d0cc05ff29
bebofofum <bri.boehm@gmail.com>
2021-07-31 12:41:19 -0400

    Renamed event handler to be more informative

--- react-omdb-challenge/src/components/MovieList.js
+++ react-omdb-challenge/src/components/MovieList.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import MovieListItem from './MovieListItem'
 
-const MovieList = ({ movies, onClick }) => {
+const MovieList = ({ movies, onNominateClick }) => {
     return(
         <div className="mt-6">
             <h1 className="text-3xl font-semibold pb-4 text-yellow-600">Movie Title</h1>
-            <ul className="flex space-x-4 px-4 justify-around">{movies.map((movie, index) => <MovieListItem itemID={index} key={index} movie={movie} onClick={onClick} />)}</ul>
+            <ul className="flex space-x-4 px-4 justify-around">{movies.map((movie, index) => <MovieListItem itemID={index} key={index} movie={movie} onNominateClick={onNominateClick} />)}</ul>
         </div>
     )
 }
