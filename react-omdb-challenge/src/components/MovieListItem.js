def176c0fc0d65d100467274fd20faffa1adfc73
bebofofum <bri.boehm@gmail.com>
2021-07-31 13:04:51 -0400

    Experimenting with the data object being passed as props

--- react-omdb-challenge/src/components/MovieListItem.js
+++ react-omdb-challenge/src/components/MovieListItem.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const MovieListItem = ({itemID, movie, onClick }) => {
+//for the onClick callback I originally tried passing the itemId to the onNominatedClick. 
+// now I'm trying with passing the movie and see what this gives me
+
+const MovieListItem = ({itemID, movie, onNominateClick }) => {
     return(
         <div className="bg-gradient-to-t from-black via-pink-900 to-pink-700  h-72 w-28 md:w-1/4 md:rounded-3xl rounded-full shadow-md relative flex flex-col items-center justify-between md:items-start py-5 transition-all duration-150">
             <li className="text-yellow-300 capitalize text-xl font-bold m-2 px-3 pt-3">{movie.Title}</li>
@@ -8,7 +11,7 @@ const MovieListItem = ({itemID, movie, onClick }) => {
                 <div className="m-2 h-15 w-full place-content-center bg-indigo-800 border-solid border-2 border-light-blue-500 mb-1 rounded-md" >
                 <button 
                     className="p-2 w-full text-center text-white capitalize "
-                    onClick={() => onClick(itemID)}>
+                    onClick={() => onNominateClick(movie)}>
                     Nominate 
                 </button>
                 </div>
