e0b10f197f9adb631a9326e19b2b96df43cb8463
bebofofum <bri.boehm@gmail.com>
2021-07-31 13:19:42 -0400

    Building out the Nominated movie list component.

--- react-omdb-challenge/src/components/NominatedMoviesList.js
+++ react-omdb-challenge/src/components/NominatedMoviesList.js
@@ -1,13 +1,39 @@
 import React from 'react'
+import '../NominatedMovies.css';
 
-const NominatedMovies = ({ nominatedMovies }) => {
+const NominatedMoviesList = ({ nomimatedMoviesArray }) => {
+
+    const renderMovieList = () => {
+        if (!nomimatedMoviesArray.name) {
+
+        } else {
+            
+        }
+        let nominatedMovies = nomimatedMoviesArray
+
+        if (nominatedMovies) {
+            return (
+                <>
+                    <ul className="nominated-movies-list">
+                        {nominatedMovies.map(movie => <li>{movie.name}</li>)}
+                    </ul>      
+                </>
+            )
+        } else {
+            return (
+                <h2 className='no-nomation-text'>No nominated movies yet.</h2>
+            )
+        }
+
+    }
+
     return(
         <div>
             <h1 className="text-3xl font-semibold pb-4 pt-6 text-yellow-600">Nominated Movies</h1>
-            <ul className="flex space-x-4 px-4 justify-around">{nominatedMovies.map((movie, index) => <li>{movie.name}</li>)}</ul>
+            <div>{renderMovieList()}</div>
         </div>
     )
 
 }
 
-export default NominatedMovies
+export default NominatedMoviesList
